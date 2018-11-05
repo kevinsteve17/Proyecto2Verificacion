fcc033d66ae0a21a4fdb3a53265a131dc84fbe18
kevinsteve17 <kevinsteve17@gmail.com>
2018-11-05 16:56:25 -0600

    changed test

--- sdr_ctrl/trunk/verif/tb/Test.sv
+++ sdr_ctrl/trunk/verif/tb/Test.sv
@@ -7,12 +7,21 @@ program testcase(inft_sdrcntrl intf);
          
   initial
     begin
-
     $display("-------------------------------------- ");
     $display(" Case-1: Single Write/Read Case        ");
     $display("-------------------------------------- ");
+    RESETN    = 1'h1;
+    env.drv.reset(RESETN);
+    #100
+    // Applying reset
+    RESETN    = 1'h0;
+    env.drv.reset(RESETN);
+    #10000;
+    // Releasing reset
+    RESETN    = 1'h1;
+    env.drv.reset(RESETN);
+    #1000;
     // single write and single read
-    env.drv.reset();
     env.drvr.BurstWrite(32'h4_0000,8'h4);
     #1000;
     burst_read();
