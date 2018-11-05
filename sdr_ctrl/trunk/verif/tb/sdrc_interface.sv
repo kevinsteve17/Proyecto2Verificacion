6108000f9de39aeeebabc6c06b648dae8ac18ac2
fparisk <fpariskikut@gmail.com>
2018-11-04 21:34:39 -0600

    build error fix 1

--- sdr_ctrl/trunk/verif/tb/sdrc_interface.sv
+++ sdr_ctrl/trunk/verif/tb/sdrc_interface.sv
@@ -1,7 +1,7 @@
 interface inft_sdrcntrl #(parameter SDR_DW = 16,
                           parameter SDR_BW = 2,
                           parameter dw = 32,
-                          parameter APP_AW = 26);
+                          parameter APP_AW = 26)
                           (input resetn, sys_clk);
 
     intf_sdram #(.SDR_DW(SDR_DW), .SDR_BW(SDR_BW)) sdram_intf ();
