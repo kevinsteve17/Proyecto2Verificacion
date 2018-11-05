36d955a26ea75ffc6e62bb46503c27c40ffdb151
Fernando Enrique Paris Kikut <ESTUDIANTES\fparis@IE-CentOS-01.estudiantes.ie.tec.ac.cr>
2018-11-04 20:46:30 -0600

    top: interface instance build issues fix

--- sdr_ctrl/trunk/verif/tb/csrc/filelist.cu
+++ sdr_ctrl/trunk/verif/tb/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _7226_archive_1.so
-_7226_archive_1.so : archive.6/_7226_archive_1.a
+ARCHIVE_OBJS += _32529_archive_1.so
+_32529_archive_1.so : archive.7/_32529_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_7226_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_32529_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_7226_archive_1.so $@
+	@ln -sf .//../simv.daidir//_32529_archive_1.so $@
 
 
 
