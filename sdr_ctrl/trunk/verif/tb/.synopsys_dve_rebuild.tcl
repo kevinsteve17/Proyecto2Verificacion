35d707836de237e7b0f8646de111ae2ff27e56d5
Kevin Steve Viquez Gomez <ESTUDIANTES\kviquez@PoolCentOS604.estudiantes.ie.tec.ac.cr>
2018-11-08 00:56:18 -0600

    updated interfacessss

--- sdr_ctrl/trunk/verif/tb/.synopsys_dve_rebuild.tcl
+++ sdr_ctrl/trunk/verif/tb/.synopsys_dve_rebuild.tcl
@@ -1,5 +1,5 @@
 # DVE Simulation Rebuild/Restart Options
-# Saved on Wed Nov 7 23:24:35 2018
+# Saved on Thu Nov 8 00:22:57 2018
 set SIMSETUP::REBUILDOPTION 1
 set SIMSETUP::REBUILDCMD {}
 set SIMSETUP::REBUILDDIR {}
