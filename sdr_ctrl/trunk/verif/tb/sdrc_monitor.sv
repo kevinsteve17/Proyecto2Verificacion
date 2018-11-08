06d416279e8f9d62caed2099810a0f393847bfdf
fparisk <fpariskikut@gmail.com>
2018-11-08 16:13:54 -0600

    more updates

--- sdr_ctrl/trunk/verif/tb/sdrc_monitor.sv
+++ sdr_ctrl/trunk/verif/tb/sdrc_monitor.sv
@@ -15,8 +15,12 @@ class sdrcMon;
         begin
             if (this.execTestCasesCount==0 && sb.ErrCnt==0) begin
                 $display("TEST EXECUTION PASSED!!!");
+                $display("  TestCase execution count: %d", this.execTestCasesCount);
+                $display("  Error count: %d", sb.ErrCnt);
             end else begin
                 $display("TEST EXECUTION FAILED!!!");
+                $display("  TestCase execution count: %d", this.execTestCasesCount);
+                $display("  Error count: %d", sb.ErrCnt);
             end
         end
     endtask
