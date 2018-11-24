50d75e1ff643a38ef4c3346bf8cadf285bfd28d8
kevinsteve17 <kevinsteve17@gmail.com>
2018-11-24 11:03:27 -0600

    2 wb assertion

--- sdr_ctrl/trunk/verif/tb/assertions.sv
+++ sdr_ctrl/trunk/verif/tb/assertions.sv
@@ -1,6 +1,6 @@
 module whitebox(intf_whitebox whitebox_if);
 
-//Inmediate assertion
+//Inmediate assertions
   always_comb begin
     sdram_init: assert #0 ($rose(whitebox_if.sdr_ras_n && whitebox_if.sdr_cas_n && whitebox_if.sdr_we_n) |->
                            $stable(whitebox_if.sdr_ras_n && whitebox_if.sdr_cas_n && whitebox_if.sdr_we_n)[*N_CYCLES]);
@@ -12,5 +12,18 @@ module whitebox(intf_whitebox whitebox_if);
                          $rose(whitebox_if.sdr_ras_n && whitebox_if.sdr_cas_n && whitebox_if.sdr_we_n) |->
                          ##N (!whitebox_if.sdr_ras_n && !whitebox_if.sdr_cas_n && whitebox_if.sdr_we_n);)*/
   end
+
+// Concurrent assertions
+
+// Rule 3.00
+ wb_init: assert property ( @(posedge whitebox_if.wb_clk_i) ($rose whitebox_if.wb_clk_i |=> $fell whitebox_if.wb_rst_i) else $error ("Wishbone Protocol Rule 3.00 violated");
+
+// Rule 3.05
+ wb_reset_1clk: assert property(@(posedge whitebox_if.wb_clk_i) ($rose whitebox_if.wb_clk_i |=> ##[1] $fell whitebox_if.wb_rst_i))
+
+// Rule 3.10  Seringa  ... 
+
+// Rule 
+
 
 endmodule
