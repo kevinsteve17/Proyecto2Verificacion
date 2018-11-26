d136a99b28d918da2f449dfb4c4fbfecfb400b7e
kevinsteve17 <kevinsteve17@gmail.com>
2018-11-25 20:47:44 -0600

    Modified scoreboard

--- sdr_ctrl/trunk/verif/tb/sdrc_scoreboard.sv
+++ sdr_ctrl/trunk/verif/tb/sdrc_scoreboard.sv
@@ -3,4 +3,36 @@ class sdrcSB;
     int dir[$];
     int burstLenght[$];
     int ErrCnt;
+
+
+    task store_write(arguments);
+    input [31:0] Address;
+    input [7:0]  bl;
+    begin
+        // write the data in the scoreboard
+    end
+        
+    endtask //
+
+    task store_read(arguments);
+    input [31:0] Address;
+    input [7:0]  bl;
+    begin
+        // read the data in the scoreboard
+    end
+    endtask //
+
+
+
+
+
+    task clearStore();
+        begin
+            // clear the data in the scoreboard
+            store = {};
+            dir = {};
+            burstLenght{};
+            ErrCnt = 0;
+        end
+    endtask //automatic
 endclass
