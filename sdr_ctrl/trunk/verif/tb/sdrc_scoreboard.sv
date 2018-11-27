30de8c59064f45f27873e154f2264183b044abcd
kevinsteve17 <kevinsteve17@gmail.com>
2018-11-26 23:48:26 -0600

    modified scoreboard

--- sdr_ctrl/trunk/verif/tb/sdrc_scoreboard.sv
+++ sdr_ctrl/trunk/verif/tb/sdrc_scoreboard.sv
@@ -3,22 +3,33 @@ class sdrcSB;
     int dir[$];
     int burstLenght[$];
     int ErrCnt;
+    mem_base_object mem [*];
 
 
-    task store_write(arguments);
+    task store_write();
     input [31:0] Address;
     input [7:0]  bl;
+    input [31:0] data;
     begin
+        mem_base_object  mem_obj = new();
+        mem_obj.addr = Address;
+        mem_obj.bl = bl;
+        mem_obj.data = data; // this won't work ... different types ... 
+
         // write the data in the scoreboard
+        mem[Address] = mem_obj;
     end
         
     endtask //
 
-    task store_read(arguments);
+    task store_read();
     input [31:0] Address;
     input [7:0]  bl;
+    mem_base_object  read_mem_obj = mem[Address];
     begin
         // read the data in the scoreboard
+        // return read_mem_obj;
+
     end
     endtask //
 
