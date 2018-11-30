eb000ba0684ca589fc012e385f02bda31e6e0817
Kevin Steve Viquez Gomez <ESTUDIANTES\kviquez@IE-CentOS-10.estudiantes.ie.tec.ac.cr>
2018-11-29 21:26:26 -0600

    modified assertions and env2

--- sdr_ctrl/trunk/verif/tb/assertions.sv
+++ sdr_ctrl/trunk/verif/tb/assertions.sv
@@ -50,7 +50,7 @@ endproperty
 // Property of Rule 3.35 Transfer cycle initiaiton
 property wb_termination;
   @(posedge whitebox_if.wb_clk_i)
-  $rose(whitebox_if.wb_cyc_i && whitebox_if.wb_stb_i) |=>
+  $rose(whitebox_if.wb_cyc_i && whitebox_if.wb_stb_i) |->
   $rose(whitebox_if.wb_ack_o);
  endproperty
 
@@ -71,7 +71,7 @@ wb_reset_react: assert property(wb_reset) else $error ("Wishbone Protocol Rule 3
 wb_SRW_RMW: assert property(wb_tci) else $error ("Wishbone Protocol Rule 3.25 violated");
 
 // Rule 3.35
-// wb_AND: assert property(wb_termination) else $error ("Wishbone Protocol Rule 3.35 violated");
+wb_AND: assert property(wb_termination) else $error ("Wishbone Protocol Rule 3.35 violated");
 
 
 endmodule
--- sdr_ctrl/trunk/verif/tb/sdrc_environment2.sv
+++ sdr_ctrl/trunk/verif/tb/sdrc_environment2.sv
@@ -5,9 +5,10 @@ class sdrcEnv2 extends sdrcEnv;
     //virtual inft_sdrcntrl inft;
 
     function new (virtual inft_sdrcntrl inft);
+      super.new(inft);
       $display("Creating SDRC Environment");
       //this.inft = inft;
-      super.new(inft);
+      //super.new(inft);
       super.sb = new();
       super.drv = new (inft, sb);
       super.mon = new (inft, sb);
